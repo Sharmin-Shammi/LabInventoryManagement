89eed374cadcc550c9068f4d49e99d3012c98ce3
Sharmin <sharminrahaman1508@gmail.com>
2024-11-30 20:34:15 +0600

    updateddatabase

---
+++ Database/Model/Inventory.cs
@@ -0,0 +1,6 @@
+﻿namespace Database.Model
+{
+    public class Inventory
+    {
+    }
+}
---
+++ Database/Model/User.cs
@@ -0,0 +1,7 @@
+﻿namespace Database.Model
+{
+    public class User
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+    }
+}
